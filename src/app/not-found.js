6011f514a4edb6a55aedb68833c92ed0188cd25e
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-02-11 00:09:04 +0600

    not found page added

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFoundPage = () => {
+    return (
+        <div className='flex justify-center'>
+            <h2 className='text-3xl  '> 404!!! || Not found </h2>
+        </div>
+    );
+};
+
+export default notFoundPage;
