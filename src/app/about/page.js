f9f414db59190695b9bd4a3874f1d96a109391e0
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-02-21 23:14:45 +0600

    next confit for images component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 export const metadata = {
     title:"about page",
@@ -7,6 +8,12 @@ const aboutPage = () => {
     return (
         <div>
             <h1 className='text-3xl' >About Page / route</h1>
+            <Image
+      src="https://img.freepik.com/free-vector/abstract-grunge-style-coming-soon-with-black-splatter_1017-26690.jpg?t=st=1708535265~exp=1708538865~hmac=466e98fc66ef27cca93991e98cb17b9fa8f04e167f20c151bd5a457a44596b77&w=740"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
         </div>
     );
 };
