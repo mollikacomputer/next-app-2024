d180f5ca61144e4f32961d4686055fb570cdbc34
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-02-21 22:42:08 +0600

    nav routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link href="/about"> <button className="btn btn-accent"> About </button> </Link>
+        <Link href="/contact"> <button className="btn btn-accent"> Contact </button> </Link>
+        <Link href="/news/articles"> <button className="btn btn-accent"> Article </button> </Link>
+        <Link href="/news/blogs"> <button className="btn btn-accent"> Blog </button> </Link>
+        <Link href="/"> <button className="btn btn-accent"> Home </button> </Link>
+        {children}
+        </body>
     </html>
   );
 }
